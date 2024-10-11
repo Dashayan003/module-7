ef7aa39265801d46fa3d4cb84f6d14202e25d1b5
Dariya <165070852+Dashayan003@users.noreply.github.com>
2024-10-11 23:09:01 +0700

    Add files via upload

---
+++ Позиционирование в файле.py
@@ -0,0 +1,20 @@
+def custom_write(file_name, strings):
+    file_name = 'test.txt'
+    strings_positions = {}
+    with open(file_name, 'w', encoding='utf-8') as file:
+        for i, string in enumerate(strings, start=1):
+            strings_positions[(i, file.tell())] = string
+            file.write(string + '\n')
+    return strings_positions
+
+
+info = [
+    'Text for tell.',
+    'Используйте кодировку utf-8.',
+    'Because there are 2 languages!',
+    'Спасибо!'
+]
+
+result = custom_write('test.txt', info)
+for elem in result.items():
+    print(elem)
